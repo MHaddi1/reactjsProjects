282d0ef4391f22d775f46c9a1cad3f1b297d9d1e
Haddi <muhammadhaddibro@gmail.com>
2022-06-22 18:12:35 -0700

    adding The Routing With 6.3.0 version

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
+import Navbar from './components/Navbar';
+function App() {
+  return (
+    <>
+     <BrowserRouter>
+   <Navbar/>
+   
+   <Routes>
+    
+    <Route path='/' element={<> <Home /> </>}></Route>
+    <Route path='About' element={<About />}></Route>
+   </Routes>
+   </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
